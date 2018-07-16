f06987372334c85e69273d8ffd5c33a73d784e54
Nathan Walters <nwalters512@gmail.com>
2018-07-15 23:08:06 -0700

    Add all old slack functionality

--- src/slack.js
+++ src/slack.js
@@ -1,5 +1,28 @@
-module.exports.start = msg => console.log(`[slack] ${msg}`);
+const rp = require('request-promise-native');
 
-module.exports.message = msg => console.log(`[slack] ${msg}`);
+let enabled = false;
 
-module.exports.enable = () => console.log('Slack enabled!');
+const sendMessage = (prefix = '') => (message, attachment = null) => {
+  if (!enabled) return;
+  rp({
+    method: 'POST',
+    uri: 'https://hooks.slack.com/services/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',
+    body: {
+      text: `${prefix} ${message}`,
+      attachments: attachment && [{ text: attachment }],
+    },
+    json: true
+  });
+};
+
+module.exports = {
+  enable: () => { enabled = true; },
+  success: sendMessage(':heavy_check_mark:'),
+  failure:sendMessage(':x:'),
+  unknown: sendMessage(':question:'),
+  start: sendMessage(':arrow_forward:'),
+  warning: sendMessage(':warning:'),
+  error: sendMessage(':heavy_exclamation_mark: <!channel>'),
+  message: sendMessage(),
+  finished: sendMessage(':tada:'),
+};
