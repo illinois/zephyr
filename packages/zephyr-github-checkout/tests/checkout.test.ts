aca6a9249ec0a99777fe2d47dca364e88b065c94
Nathan Walters <nwalters512@gmail.com>
2018-07-20 22:20:32 -0700

    Fix checkout tests

--- packages/zephyr-github-checkout/tests/checkout.test.ts
+++ packages/zephyr-github-checkout/tests/checkout.test.ts
@@ -1,8 +1,8 @@
 /* eslint-env jest */
 
 import mockFs from 'jest-plugin-fs';
-import octokit from '../src/octokit';
-import checkout from '../src/checkout';
+import Octokit from '@octokit/rest';
+import checkout, { ICheckoutOptions } from '../src/index';
 
 const MOCK_COMMITS_SHA = 'fedcba';
 const MOCK_REFERENCE_SHA = 'abcdef';
@@ -11,7 +11,8 @@ jest.mock('fs', () => require('jest-plugin-fs/mock'));
 jest.mock('request-promise-native', () => {
   return jest.fn(() => Promise.resolve('content'));
 });
-jest.mock('../src/octokit', () => {
+
+const mockOctokit = (): Octokit => {
   const mockGithubFiletree = require('./util/mock-github-filetree')();
   const getContentMock = jest.fn().mockImplementation(async (opts) => {
     const data = await mockGithubFiletree(opts);
@@ -23,19 +24,17 @@ jest.mock('../src/octokit', () => {
   const getReferenceMock = () => Promise.resolve({
     data: { object: { sha: MOCK_REFERENCE_SHA } }
   });
-  const mock = jest.fn(() => {
-    return {
-      repos: {
-        getContent: getContentMock,
-        getCommits: getCommitsMock,
-      },
-      gitdata: {
-        getReference: getReferenceMock,
-      }
-    };
-  });
-  return mock;
-});
+  // @ts-ignore: We don't need to mock out all of GitHub
+  return {
+    repos: {
+      getContent: getContentMock,
+      getCommits: getCommitsMock,
+    },
+    gitdata: {
+      getReference: getReferenceMock,
+    }
+  }
+}
 
 beforeEach(() => {
   jest.clearAllMocks();
@@ -46,12 +45,13 @@ afterEach(() => {
   mockFs.restore();
 });
 
-const REPO_OPTIONS = { org: 'test', repo: 'test' };
+const REPO_OPTIONS = { owner: 'test', repo: 'test' };
 
 describe('checkout', () => {
   it('checks out the entire repo', async () => {
-    const options = {
+    const options: ICheckoutOptions = {
       ...REPO_OPTIONS,
+      octokit: mockOctokit(),
       repoPath: '',
       checkoutPath: '/test',
     };
@@ -70,6 +70,7 @@ describe('checkout', () => {
   it('checks out the .gitignore file', async () => {
     const options = {
       ...REPO_OPTIONS,
+      octokit: mockOctokit(),
       repoPath: '',
       checkoutPath: '/test',
       files: [{ name: '.gitignore', required: true }],
@@ -85,6 +86,7 @@ describe('checkout', () => {
   it('checks out the mp1 directory', async () => {
     const options = {
       ...REPO_OPTIONS,
+      octokit: mockOctokit(),
       repoPath: 'mp1',
       checkoutPath: '/test',
     };
@@ -101,6 +103,7 @@ describe('checkout', () => {
   it('checks out specific files in the mp1 directory', async () => {
     const options = {
       ...REPO_OPTIONS,
+      octokit: mockOctokit(),
       repoPath: 'mp1',
       checkoutPath: '/test',
       files: [{ name: 'mp1.cpp', required: true }],
@@ -116,6 +119,7 @@ describe('checkout', () => {
   it ('uses the provided timestamp to determine which revision to fetch', async () => {
     const options = {
       ...REPO_OPTIONS,
+      octokit: mockOctokit(),
       repoPath: 'mp1',
       checkoutPath: '/test',
       timestamp: '2017-10-01T01:54:55+00:00'
@@ -133,6 +137,7 @@ describe('checkout', () => {
   it ('uses the provided timestamp to determine which revision to fetch', async () => {
     const options = {
       ...REPO_OPTIONS,
+      octokit: mockOctokit(),
       repoPath: 'mp1',
       checkoutPath: '/test',
       ref: 'deadbeef'
@@ -148,42 +153,48 @@ describe('checkout', () => {
   });
 
   it('pins all request to the same ref', async () => {
+    const octokit = mockOctokit();
     const options = {
       ...REPO_OPTIONS,
+      octokit,
       repoPath: 'mp1',
       checkoutPath: '/test',
     };
 
     await checkout(options);
-    (octokit() as any).repos.getContent.mock.calls.forEach((call: any) => {
+    (octokit as any).repos.getContent.mock.calls.forEach((call: any) => {
       expect(call[0].ref).toEqual(MOCK_REFERENCE_SHA);
     });
   });
 
   it('pins all request to the same ref (with timestamp)', async () => {
+    const octokit = mockOctokit();
     const options = {
       ...REPO_OPTIONS,
+      octokit,
       repoPath: 'mp1',
       checkoutPath: '/test',
       timestamp: '2017-10-01T01:54:55+00:00'
     };
 
     await checkout(options);
-    (octokit() as any).repos.getContent.mock.calls.forEach((call: any) => {
+    (octokit as any).repos.getContent.mock.calls.forEach((call: any) => {
       expect(call[0].ref).toEqual(MOCK_COMMITS_SHA);
     });
   });
 
   it('pins all request to the same ref (with ref)', async () => {
+    const octokit = mockOctokit();
     const options = {
       ...REPO_OPTIONS,
+      octokit,
       repoPath: 'mp1',
       checkoutPath: '/test',
       ref: 'deadbeef'
     };
 
     await checkout(options);
-    (octokit() as any).repos.getContent.mock.calls.forEach((call: any) => {
+    (octokit as any).repos.getContent.mock.calls.forEach((call: any) => {
       expect(call[0].ref).toEqual('deadbeef');
     });
   });
---
+++ packages/zephyr-github-checkout/tests/types.d.ts
@@ -0,0 +1,6 @@
+declare module 'jest-plugin-fs' {
+    export let restore: any;
+    export let files: any;
+    export let mock: any;
+  }
+  
